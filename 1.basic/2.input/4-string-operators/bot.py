c3ab18a83f70ef8ebf710de277095177dd8f19ed
ryansc1997 <2shepr69@solent.ac.uk>
2019-09-25 10:53:16 +0000

    input

---
+++ 1.basic/2.input/4-string-operators/bot.py
@@ -0,0 +1,17 @@
+
+num1 = int(input(" Please enter the number of lives. "))
+
+print()
+num2 = int(input(" Please enter the energy level. "))
+
+print()
+
+num3 = int(input(" Please enter the shield level. "))
+
+print()
+
+print("Health has been set.")
+
+print(" Lives: " + "♥"*num1 )
+print(" Energy: " + "♦"*num2 )
+print(" Lives: " + "♦"*num3 )
