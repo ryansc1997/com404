f11f4fca989c4cb1fbb7d274fc4fc28241b2d376
ryansc1997 <2shepr69@solent.ac.uk>
2019-09-25 10:02:49 +0000

    task 1

--- 1.basic/2.input/1.user-input/bot.py
+++ 1.basic/2.input/1.user-input/bot.py
@@ -1,4 +1,4 @@
 # Ask user to enter their name
 print("What is your name human?")
-name = input()
-print("It is nice to meet you human", name)
+name = input("Ryan")
+print("Nice to meet you " + name + ".")
