7e97b3c3ca7067fae4a52a80280cd9863c45cc43
ryansc1997 <2shepr69@solent.ac.uk>
2019-09-25 10:35:14 +0000

    data types

---
+++ 1.basic/2.input/data-types/bot.py
@@ -0,0 +1,19 @@
+name = "Ryan" 
+age = 22
+height = 1.8
+weight = 80
+
+
+print("What is your name Human?")
+print(name)
+print()
+print("How old are you (in years)?")
+print(age)
+print()
+print("How tall are you (in meters)?")
+print(height)
+print()
+print("How much do you weigh (in kilograms)?")
+print(weight)
+print()
+print( name + " you are " + str(age) + " years old and your bmi is 24.7")
