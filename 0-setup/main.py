afeadffbec1856ee41e42a2a05a758241704341a
ryansc1997 <2shepr69@solent.ac.uk>
2019-09-25 09:47:07 +0000

    task 2 complete

---
+++ 0-setup/main.py
@@ -0,0 +1,12 @@
+
+
+name = "Ryan" 
+age = 22
+height = 1.8
+initial = 'R'
+isAwesome = True
+
+print ("hello  ")
+print(name)
+print("hello, " + name)
+print("hello, " + name + " you are " + str(age) + " years old")
